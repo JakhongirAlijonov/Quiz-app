bdecbcef0d28ddc800c8b32c4f7769b5209478a5
jakhongiralijonov <jakhongirweb@gmail.com>
2023-01-05 15:38:26 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,117 @@
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonElement = document.getElementById('answer-buttons')
+const nextButton = document.getElementById('next-btn')
+const count = document.getElementById("count")
+let shuffledQuestions, currentQuestionindex 
+let counter = 0;
+
+startButton.addEventListener("click", startGame)
+nextButton.addEventListener('click' , ()=>{
+    currentQuestionindex++
+    setNextQuestion()
+})
+
+function startGame(){
+startButton.classList.add("hide")
+questionContainerElement.classList.remove("hide")
+shuffledQuestions = questions.sort(()=>Math.random() - .5)
+currentQuestionindex = 0
+setNextQuestion()
+}
+
+
+function setNextQuestion(){
+resetState()
+showQuestion(shuffledQuestions[currentQuestionindex])
+
+}
+
+
+function resetState(){
+    clearrStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while(answerButtonElement.firstChild){
+        answerButtonElement.removeChild(answerButtonElement.firstChild)
+    }
+}
+
+function showQuestion(question){
+    questionElement.innerText = question.question
+    question.answers.forEach(answer=>{
+        const button = document.createElement('button')
+        button.innerHTML = answer.text
+        button.classList.add('btn')
+        if(answer.correct){
+            button.dataset.correct = answer.correct
+        }
+
+        button.addEventListener('click' , selectAnswer)
+        answerButtonElement.appendChild(button)
+    })
+}
+
+function selectAnswer(e){
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body , correct)
+    Array.from(answerButtonElement.children).forEach(button=>{
+        setStatusClass(button , button.dataset.correct)
+    })
+if(shuffledQuestions.length > currentQuestionindex +1){
+    nextButton.classList.remove('hide')
+} else {
+    startButton.innerText = 'Restart'
+     startButton.classList.remove('hide')
+}
+}
+
+function setStatusClass(element , correct){
+    clearrStatusClass(element)
+if(correct){
+    element.classList.add('correct')
+} else element.classList.add('wrong')
+}
+
+function clearrStatusClass(element){
+    element.classList.remove('correct')
+
+    element.classList.remove('wrong')
+}
+
+
+
+const questions = [
+    {
+        question: 'What is 2+2',
+        answers:[
+            {text:'4' ,correct:true},
+            {text:'22' ,correct:false}
+        ]
+    },
+    {
+        question: 'What is 2+3',
+        answers:[
+            {text:'5' ,correct:true},
+            {text:'25' ,correct:false}
+        ]
+    },
+    {
+        question: 'What is 2+3',
+        answers:[
+            {text:'5' ,correct:true},
+            {text:'25' ,correct:false}
+        ]
+    },
+    {
+        question: 'What is 5*10',
+        answers:[
+            {text:'50' ,correct:true},
+            {text:'250' ,correct:false},
+            {text:'100' ,correct:false},
+            {text:'150' ,correct:false}
+        ]
+    }
+
+]
